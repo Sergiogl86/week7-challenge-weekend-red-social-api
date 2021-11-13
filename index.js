3ed723289e7942b5f21c5eb00bf140434cf56eb6
Sergio Garcia <sergio.gl86@gmail.com>
2021-11-13 09:20:52 +0100

    Se me olvido crear master

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { initializeServer } = require("./server/index");
 const initializeMongoDBServer = require("./database/index");
 
-const port = process.env.PORT || process.env.LOCAL_PORT || 5000;
+const port = process.env.PORT || process.env.LOCAL_PORT || 5050;
 
 (async () => {
   await initializeServer(port);
